9796cdfc1ea8c81e0923811ebec205c6bb05095b
Muskan <muskan.mangoit@gmail.com>
2022-12-19 19:15:15 +0530

    current port kill and restart

--- node/app.js
+++ node/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const http = require('http');
 const path = require('path');
+const kill = require("kill-port");
   
 app.get("/gettry", (req, res) => {
   res.send("Hello World!");
@@ -23,6 +24,20 @@ app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
 
 const server = http.createServer(app);
 
+server.once('error', function(err) {
+  if (err.code === 'EADDRINUSE') {
+    // port is currently in use
+	kill(PORT, "tcp");
+	console.log(`${PORT} is currently in use now it's kill,Please restart the server`)
+  }
+});
+
+server.once('listening', function() {
+  // close the server if listening doesn't fail
+  server.close();
+});
+
 server.listen(PORT, () => console.log(`Server started on port ${PORT} on http://${host}:${PORT}`));
+
 
 
